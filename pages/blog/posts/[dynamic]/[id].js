16891a59953eef250a24ee599e55267ece158bf2
amalkhairin <amallanch45@gmail.com>
2022-04-17 23:48:54 +0700

    fix bug

--- pages/blog/posts/[dynamic]/[id].js
+++ pages/blog/posts/[dynamic]/[id].js
@@ -1,11 +1,19 @@
 import { getAllPostIds, getPostData } from '../../../../lib/posts'
 import Link from 'next/link'
+import Head from 'next/head'
 import * as Constant from '../../../../src/constant.js'
 import Image from 'next/image'
 import profile_pict from '../../../../public/profile_pict.jpeg'
 
 export default function Post({ postData }) {
   return (
+      <div>
+      <Head>
+        <title>{postData.data.title} | {postData.data.author}</title>
+        <meta name="description" content={postData.data.body.substring(0,15)} />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
       <div className='container post-bg'>
         <div className='content'>
             <div style={{display: 'flex', alignItems: 'center'}}>
@@ -18,15 +26,18 @@ export default function Post({ postData }) {
                 </div>
                 <div className='px-3' style={{alignItems: 'center'}}>
                     <p className='content-author pt-2'>Posted on {postData.data.created_at}</p>
-                    <p className='content-author pb-2' style={{color: 'blue'}}>by <a href={Constant.LINKED_IN_URL} target="_blank" rel='noreferrer'>
-                        {postData.data.author} </a>
-                    </p>
+                    <Link href={{pathname: "/"}} passHref>
+                        <p className='content-author pb-2 link' style={{color: 'blue'}}>
+                            by {postData.data.author}
+                        </p>
+                    </Link>
                 </div>
             </div>
             <h1 className='content-title pb-2'>{postData.data.title}</h1>
             <p className='content-body'>{postData.data.body}</p>
         </div>
       </div>
+    </div>
   )
 }
 
