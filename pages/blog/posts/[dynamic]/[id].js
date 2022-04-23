296df1fe2ce1ec99d5a1c545103db10483f73e26
amalkhairin <amallanch45@gmail.com>
2022-04-23 15:34:35 +0700

    update metadate post page

--- pages/blog/posts/[dynamic]/[id].js
+++ pages/blog/posts/[dynamic]/[id].js
@@ -11,21 +11,14 @@ export default function Post({ postData }) {
       <div>
       <Head>
         <title>{postData.data.title} | {postData.data.author}</title>
-        <meta name="description" content={postData.data.body.substring(0,15)} />
+        <meta name="description" content={postData.data.excerpt.substring(0,15)} />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico" />
     </Head>
       <div className='container post-bg'>
         <div className='content'>
             <div style={{display: 'flex', alignItems: 'center'}}>
-                <div className='avatar' style={{alignItems: 'center'}}>
-                    <Link href={{pathname: "/"}} passHref>
-                        <button>
-                            <Image src={profile_pict} alt="Avatar" width='50vw' height='50vw' className='profile-blog' />
-                        </button>
-                    </Link>
-                </div>
-                <div className='px-3' style={{alignItems: 'center'}}>
+                <div className='' style={{alignItems: 'center'}}>
                     <p className='content-author pt-2'>Posted on {postData.data.created_at}</p>
                     <Link href={{pathname: "/"}} passHref>
                         <p className='content-author pb-2 link' style={{color: 'blue'}}>
